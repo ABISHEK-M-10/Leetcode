bec20e785d1eff1250230756a6673217abddc213
ABISHEK MURUGESAN <abishekm112004@gmail.com>
2025-11-17 18:53:53 +0530

    Time: 3 ms (14.29%) | Memory: 65.6 MB (9.43%) - LeetSync

---
+++ 1548-check-if-all-1s-are-at-least-length-k-places-away/check-if-all-1s-are-at-least-length-k-places-away.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean kLengthApart(int[] nums, int k) {
+        int count=k;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]==1)
+            {
+                if(count<k)
+                {
+                    System.out.print(count);
+                    return false;
+                }
+                count = 0;
+            }
+            if(nums[i]==0)
+            {
+                count+=1;
+            }
+        }
+        return true;
+    }
+}
