e4249f7faafb44950a2b4956806b1d093e18b42c
ABISHEK MURUGESAN <abishekm112004@gmail.com>
2025-12-25 19:45:30 +0530

    Time: 42 ms (70.97%) | Memory: 109 MB (65.05%) - LeetSync

---
+++ 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.java
@@ -0,0 +1,18 @@
+class Solution {
+    public long maximumHappinessSum(int[] happiness, int k) {
+        Arrays.sort(happiness);
+        long res = 0;
+
+        for(int i=0;i<k;i++)
+        {
+            int val = happiness[happiness.length -1 -i] - i;
+            if(val<=0)
+            {
+                return res;
+            }
+            res += val;
+        }
+
+        return res;
+    }
+}
