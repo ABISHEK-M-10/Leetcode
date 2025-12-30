3f688cdaa84570e05fddc85faa94edca8a0908fa
ABISHEK MURUGESAN <abishekm112004@gmail.com>
2025-12-30 09:19:56 +0530

    Time: 6 ms (52.16%) | Memory: 43.5 MB (18.88%) - LeetSync

---
+++ 409-longest-palindrome/longest-palindrome.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int longestPalindrome(String s) {
+        HashSet<Character> set = new HashSet<>();
+
+        int count = 0;
+
+        for(int i=0;i<s.length();i++)
+        {
+            char ch = s.charAt(i);
+
+            if(set.contains(ch))
+            {
+                set.remove(ch);
+                count++;
+            }
+            else
+            {
+                set.add(ch);
+            }
+        }
+        if(!set.isEmpty())
+        {
+            return count*2 + 1;
+        }
+
+        return count*2;
+    }
+}
